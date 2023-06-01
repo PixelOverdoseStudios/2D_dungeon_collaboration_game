ebeb67723ae56ce962341f7189dcce2a30b4cdd3
PixelOverdoseStudios <123806965+PixelOverdoseStudios@users.noreply.github.com>
2023-06-01 01:00:36 -0500

    Player Added

---
+++ 2D Dungeon Collaboration Game/Assets/Chris/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = moveInput * moveSpeed;
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+    }
+}
